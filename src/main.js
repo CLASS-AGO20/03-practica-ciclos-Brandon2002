ff8c5675e1339d241e0f827c0daac2e921976fd2
Brandon2002 <btapia@ucol.mx>
2020-12-10 15:49:19 -0600

    implemented factorial()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-export default class App {}
+export default class App {
+    factorial(numero){
+        let suma = 1;
+        for(let i = 2; i <= numero; i++){
+            suma = suma * i;
+        }
+        return suma;
+    }
+}
 
 let app = new App();
+console.log(app.factorial(10));
